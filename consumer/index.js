7e490d8f8147cfbf6c7b92f1fed9fa7b4193281a
Andre Macedo <andre.macedo@seidor.com.br>
2021-07-22 15:14:47 -0300

    refactor: improve consumer to get all the batch size despite sqs limitations

--- consumer/index.js
+++ consumer/index.js
@@ -11,7 +11,7 @@ const baseURL = `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${process.e
 const receiveMessages = async (queueName, batchsize) => {
   const params = {
     QueueUrl: `${baseURL}${queueName}`,
-    MaxNumberOfMessages: batchsize,
+    MaxNumberOfMessages: batchsize > 10 ? 10 : batchsize,
   };
 
   return sqs.receiveMessage(params).promise();
@@ -30,23 +30,31 @@ const deleteMessages = async (queueName, messages) => {
   return sqs.deleteMessageBatch(params).promise();
 };
 
-const processMessages = async (messages) => {
+const processMessages = async (queueName, messages) => {
+  console.log(`processing ${messages.length} messages from ${queueName}`);
   messages.forEach((message) => console.log(message.Body));
 };
 
-const pollMessages = async (queue) => {
-  const data = await receiveMessages(queue.name, queue.batchsize);
-  if (!data.Messages) return;
+const pollMessages = async (queueName, batchsize, emptyCount = 0) => {
+  const data = await receiveMessages(queueName, batchsize);
+  if (!data.Messages) {
+    if (emptyCount > 0) return;
+    else return pollMessages(queueName, batchsize, ++emptyCount);
+  }
 
-  await processMessages(data.Messages);
+  await processMessages(queueName, data.Messages);
 
-  deleteMessages(queue.name, data.Messages);
+  deleteMessages(queueName, data.Messages);
+
+  if (data.Messages.length < batchsize) {
+    pollMessages(queueName, batchsize - data.Messages.length);
+  }
 };
 
 const main = async () => {
   const promises = [];
   for (const queue of queues) {
-    promises.push(pollMessages(queue));
+    promises.push(pollMessages(queue.name, queue.batchsize));
   }
 
   await Promise.all(promises);
